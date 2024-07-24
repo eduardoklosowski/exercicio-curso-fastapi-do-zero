18cb596224a4809aa32d94ee69ebf9d687e4cac2
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-07-24 01:24:28 -0300

    Exercício 1: Endpoint que responde HTML e teste

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -10,3 +11,16 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_html():
+    return """
+    <html>
+        <head>
+            <title>Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo!</h1>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,13 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_html_deve_retornar_ok_e_pagina_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<title>Nosso olá mundo!</title>' in response.text
+    assert '<h1>Olá Mundo!</h1>' in response.text
