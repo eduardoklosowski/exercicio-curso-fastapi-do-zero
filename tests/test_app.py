3ec551477743fdcf0c57255960f6fd4817c570ac
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-07-24 00:35:03 -0300

    Testa Hello Word

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fast_zero.app import app
+from fastapi.testclient import TestClient
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
