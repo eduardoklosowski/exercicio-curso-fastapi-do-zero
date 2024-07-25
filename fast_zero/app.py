254b9c8f4a2aafddc3b0261b29a6383604311e09
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-07-25 00:42:40 -0300

    Exercício 3: Endpoint para pegar apenas um usuário

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -42,6 +42,14 @@ def read_users():
     return {'users': database}
 
 
+@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    return database[user_id - 1]
+
+
 @app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
--- tests/test_app.py
+++ tests/test_app.py
@@ -49,6 +49,24 @@ def test_read_users(client):
     }
 
 
+def test_read_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_user_com_id_invalido(client):
+    response = client.get('/users/0')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_update_user(client):
     response = client.put(
         '/users/1',
