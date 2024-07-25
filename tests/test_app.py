0c90236d2a1d079c5161b27268800ffa7ea56042
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-07-25 02:57:44 -0300

    Exercício 2: Teste de ciar usuário com mesmo e-mail

--- tests/test_app.py
+++ tests/test_app.py
@@ -35,6 +35,7 @@ def test_create_user(client):
         'id': 1,
     }
 
+
 def test_create_user_com_mesmo_username(client, user):
     response = client.post(
         '/users/',
@@ -48,6 +49,19 @@ def test_create_user_com_mesmo_username(client, user):
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert response.json() == {'detail': 'Username already exists'}
 
+
+def test_create_user_com_mesmo_email(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': user.email,
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists'}
 
 
 def test_read_users_sem_usuarios(client):
